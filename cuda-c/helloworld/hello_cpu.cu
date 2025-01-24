d22288f2423635e952ead84ac30f0b0e1c4aa878
Jesse3692 <lcf.zhangxing@hotmail.com>
2025-01-24 16:19:02 +0800

    feat: cuda-c hello

---
+++ cuda-c/helloworld/hello_cpu.cu
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+__global__ void helloFromGPU(void){
+    printf("Hello World from GPU!\n");
+}
+
+int main(void){
+    // hello from cpu
+    printf("Hello World from CPU!\n");
+
+    // hello from gpu
+    helloFromGPU<<<1, 10>>>();
+    cudaDeviceReset();
+    return 0;
+}
